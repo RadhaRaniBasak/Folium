74ffc9dc66c9ac53fc39eb3e43df25c1d3a1efd8
Radharani Basak <radharanibasak2003@gmail.com>
2026-04-20 02:20:18 +0530

    Create db.js

---
+++ apps/server/dist/config/db.js
@@ -0,0 +1,29 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const vite_1 = require("vite");
+const plugin_react_1 = __importDefault(require("@vitejs/plugin-react"));
+const path_1 = require("path");
+exports.default = (0, vite_1.defineConfig)({
+    plugins: [(0, plugin_react_1.default)()],
+    resolve: {
+        alias: {
+            '@shared': (0, path_1.resolve)(__dirname, '../../packages/shared/src'),
+        },
+    },
+    server: {
+        port: 5173,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3001',
+                changeOrigin: true,
+            },
+            '/health': {
+                target: 'http://localhost:3001',
+                changeOrigin: true,
+            },
+        },
+    },
+});
