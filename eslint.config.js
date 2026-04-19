93863b45cb85df394024e9a15285ccb3242b009a
Radharani Basak <radharanibasak2003@gmail.com>
2026-04-20 01:55:39 +0530

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,14 @@
-// @ts-check
 import js from "@eslint/js";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
 import reactPlugin from "eslint-plugin-react";
 import reactHooksPlugin from "eslint-plugin-react-hooks";
 
-/** @type {import("eslint").Linter.FlatConfig[]} */
 export default [
   {
     ignores: ["**/dist/**", "**/node_modules/**", "**/.vite/**"],
   },
-  // Base JS rules
   js.configs.recommended,
-  // TypeScript files (shared + server)
   {
     files: ["packages/**/*.ts", "apps/server/**/*.ts"],
     languageOptions: {
@@ -29,7 +25,6 @@ export default [
       "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
     },
   },
-  // TypeScript + React files (client)
   {
     files: ["apps/client/**/*.{ts,tsx}"],
     languageOptions: {
