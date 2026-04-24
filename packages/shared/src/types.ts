228cd70f3faa66554ae408f7dc97d59c37952cb4
Radharani Basak <radharanibasak2003@gmail.com>
2026-04-25 00:21:00 +0530

    Update types.ts

--- packages/shared/src/types.ts
+++ packages/shared/src/types.ts
@@ -15,4 +15,25 @@ export interface User {
   id: string;
   name: string;
   email: string;
+  avatar?: string;
+}
+
+export interface AuthTokens {
+  accessToken: string;
+  refreshToken: string;
+}
+
+export interface AuthResponse extends AuthTokens {
+  user: User;
+}
+
+export interface RegisterInput {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface LoginInput {
+  email: string;
+  password: string;
 }
